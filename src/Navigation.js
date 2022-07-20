5a6008398100895b6dce5ec7d60c37f19bbc2736
Nobert <nobertdalton@gmail.com>
2022-07-20 15:07:50 -0400

    0720

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,21 +4,32 @@ import logo from "./logo.svg"
 import Button from '@mui/material/Button';
 import {SvgIcon, TextField} from "@mui/material";
 import Icon from '@mui/material/Icon';
-import ReactDOM from 'react-dom';
 
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+
+import IconButton from '@mui/material/IconButton';
+import PersonIcon from '@mui/icons-material/Person';
+import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
 class Navigation extends React.Component{
     logo ="./Logo1.png";
 
     render() {
         return(
             <div id="navigation">
-
-                <Icon>MapMarker</Icon>;
-
-
-                <Button  id="Nbutton1">find your city!</Button>
-                <TextField id="filled-basic" label="CityName" variant="filled" id="Ntext"/>
-
+                <div id="leftBar">
+                    <Button  id="Nbutton1">find your city!</Button>
+                    <TextField id="outlined-basic" label="CityName" variant="outlined"  size="small"/>
+                </div>
+                <div id="rightBar">
+                    <IconButton>
+                        <PersonIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <DeleteTwoToneIcon/>
+                    </IconButton>
+                    <FormControlLabel control={<Switch defaultChecked />} label="Full Screen"   />
+                </div>
             </div>
         )
     }
