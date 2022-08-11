c41c855e93155ba9f1d16e2a80a085e947ff9f7a
nobert <nobertdalton@gmail.com>
2022-08-11 18:29:15 -0400

    update

--- Frontend/src/Components/OriginMap/OriginMap.js
+++ Frontend/src/Components/OriginMap/OriginMap.js
@@ -5,35 +5,49 @@ import './OriginMap.css'
 
 const position = [51.505, -0.09]
 
+let mymap = null;
+var myimage = L.icon({
+    iconUrl:'../images/point.png', //图片url
+    iconSize:[12,12],     //设置图片大小 宽度50.高度100
+    iconAnchor: [0,0] ,   //设置定位点的位置。默认为中心  例子中以左上角为定位参考。相当于relative
+    popupAnchor:[50 ,0],   //设置警示框位置 ，以iconAnchor的值进行定位。相当于absolute 例子中的警示框定位到有、右上角。
+});
+
 class  OriginMap  extends React.Component{
 
-    constructor(props) {
-        super(props);
-        this.mymap=null;
-    }
+     state={
+            pointStack:[],
+        }
 
 
     handlePointTool = ()=>{
-        console.log("已经调用到这个")
-        this.mymap.on('click', function(ev) {
-            alert(ev.latlng); // ev 是一个事件对象（本例中是 MouseEvent ）
+        const {pointStack} = this.state;
+        var temp=null;
+        console.log("调用到这个PointTool")
+        mymap.on('click', function(ev) {
+            var name =pointStack.length+1;
+            var marker = L.marker(ev.latlng,).addTo(mymap);
+            //pointStack.push({[name]:temp});
+            //alert(ev.latlng); // ev 是一个事件对象（本例中是 MouseEvent ）
         });
+
     }
     handleCircleTool = ()=>{
-        console.log("已经调用到这个")
+        console.log("已经调用到CircleTool");
+
     };
 
 
     componentDidMount() {
         const{OSMUrl,CurrentState} = this.props;
-        this.mymap = L.map("originMap").setView([45.4131, -75.7026], 12);
-        L.tileLayer(OSMUrl).addTo(this.mymap);
+        mymap = L.map("originMap").setView([45.4131, -75.7026], 12);
+        L.tileLayer(OSMUrl).addTo(mymap);
 
     }
 
     componentDidUpdate(prevProps: Readonly<P>, prevState: Readonly<S>, snapshot: SS) {
         const{OSMUrl,CurrentState} = this.props;
-        L.tileLayer(OSMUrl).addTo(this.mymap);
+        L.tileLayer(OSMUrl).addTo(mymap);
         if(CurrentState ==="point"){
             this.handlePointTool();
         }else if(CurrentState ==="circle"){
