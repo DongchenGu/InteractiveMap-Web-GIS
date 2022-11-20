0746bd724954643ebdace59d692198cfb8aa9144
nobert <nobertdalton@gmail.com>
2022-11-19 21:45:36 -0500

    photoProcessing version4

--- Frontend/src/Components/Waiting/Waiting.jsx
+++ Frontend/src/Components/Waiting/Waiting.jsx
@@ -1,30 +1,35 @@
 import React, {useEffect} from 'react'
 import "./Waiting.css"
 
+//取消默认事件
+function stopDefault(e) {
+    if (e.preventDefault) {
+        e.preventDefault();
+        e.stopPropagation();
+    } else {
+        window.event.returnValue = false;
+        e.cancelBubble = true;
+    }
+    return false;
+}
+let stopScroll=(e)=>{
+    var e = e || window.event;
+    //阻止窗口默认的滚动事件
+    stopDefault(e);
+}
+
+
 
 export  default  function Waiting(){
 
-    //取消默认事件
-    function stopDefault(e) {
-        if (e.preventDefault) {
-            e.preventDefault();
-            e.stopPropagation();
-        } else {
-            window.event.returnValue = false;
-            e.cancelBubble = true;
-        }
-        return false;
-    }
+
     /* 以下的 document.querySelector("#big-box")为页面中最外层盒子
     经测试绑定在body，html或者window上滚动会一直报这个错误
     [Intervention] Unable to preventDefault inside passive event listener due to target being treated as passive. See <URL>（由于目标被视为被动，无法在被动事件监听器中阻止缺省事件。）
     */
     //开放滚动
     function startwheel() {
-        document.querySelector("#waiting-cover").removeEventListener('DOMMouseScroll', function (e) {
-            var e = e || window.event;
-            stopDefault(e);
-        }, false);
+        document.querySelector("#waiting-cover").removeEventListener('DOMMouseScroll', stopScroll, false);
         document.querySelector("#waiting-cover").onmousewheel = null;
     }
 
@@ -32,16 +37,9 @@ export  default  function Waiting(){
     function stopwheel() {
         var str = window.navigator.userAgent.toLowerCase();
         if (str.indexOf('firefox') !== -1) { //火狐浏览器
-            document.querySelector("#waiting-cover").addEventListener('DOMMouseScroll', function (e) {
-                var e = e || window.event;
-                //阻止窗口默认的滚动事件
-                stopDefault(e);
-            }, false);
+            document.querySelector("#waiting-cover").addEventListener('DOMMouseScroll', stopScroll, false);
         } else { //非火狐浏览器
-            document.querySelector("#waiting-cover").onmousewheel = function (ev) {
-                var e = ev || window.event;
-                stopDefault(e);
-            };
+            document.querySelector("#waiting-cover").onmousewheel = stopScroll;
         }
     }
 
@@ -51,9 +49,9 @@ export  default  function Waiting(){
         stopwheel();
     },[])
     useEffect(()=>{
-        return () => {
-            startwheel();
-        }
+        // return () => {
+        //     startwheel();
+        // }
     },[])
 
 
